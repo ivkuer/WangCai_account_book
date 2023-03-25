72fb7830a05d61bc958c27d9a00d43860a620e90
rain <1063044718@qq.com>
2023-03-25 22:58:08 +0800

    封装 taglist 增删改查

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -6,7 +6,7 @@
       <span class="rightIcon"></span>
     </div>
     <div class="form-wrapper">
-      <FormItem :value="tag.name"
+      <FormItem :value="tag?.name"
                 @update:value="update"
                 field-name="标签名" placeholder="请输入标签名"/>
     </div>
@@ -19,7 +19,6 @@
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
-  import tagListModel from '@/models/tagListModel';
   import FormItem from '@/components/Money/FormItem.vue';
   import Button from '@/components/Button.vue';
 
@@ -27,29 +26,24 @@
     components: {Button, FormItem}
   })
   export default class EditLabel extends Vue {
-    tag?: { id: string, name: string } = undefined;
+    tag?: Tag = undefined;
 
     created() {
-      const id = this.$route.params.id;
-      tagListModel.fetch();
-      const tags = tagListModel.data;
-      const tag = tags.filter(t => t.id === id)[0];
-      if (tag) {
-        this.tag = tag;
-      } else {
+      this.tag = window.findTag(this.$route.params.id);
+      if (!this.tag) {
         this.$router.replace('/404');
       }
     }
 
     update(name: string) {
       if (this.tag) {
-        tagListModel.update(this.tag.id, name);
+        window.updateTag(this.tag.id, name);
       }
     }
 
     remove() {
       if (this.tag) {
-        if (tagListModel.remove(this.tag.id)) {
+        if (window.removeTag(this.tag.id)) {
           this.$router.back();
         } else {
           window.alert('删除失败');
