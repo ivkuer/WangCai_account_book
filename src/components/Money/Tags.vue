9f49c798af7b13459376ba394fed9cf45123725a
rain <1063044718@qq.com>
2023-03-26 19:08:41 +0800

    Tag组件使用store

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -20,12 +20,16 @@ import Vue from 'vue';
   @Component({
     computed: {
       tagList() {
-        // return this.$store.fetchTags()
+        return this.$store.state.tagList
       }
     }
   })
   export default class Tags extends Vue {
     selectedTags: string[] = [];
+
+    created() {
+      this.$store.commit('fetchTags')
+    }
 
     toggle(tag: string) {
       const index = this.selectedTags.indexOf(tag);
@@ -42,7 +46,7 @@ import Vue from 'vue';
       if (!name || name === '') {
         return window.alert('标签名不能为空');
       } 
-       store.createTag(name)
+       this.$store.commit('createTag', name)
 
     }
   }
